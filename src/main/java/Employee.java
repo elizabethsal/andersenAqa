469aa8feccd7e05e19260a961e01a91329a82422
elizabethsal <dasharil1997@yandex.ru>
2022-05-27 17:57:56 +0400

    Lesson_3 branch. Fixed maven for all branches.

---
+++ src/main/java/Employee.java
@@ -0,0 +1,30 @@
+
+public class Employee {
+    String name;
+    String position;
+    String email;
+    String phoneNumber;
+    int salary;
+    int age;
+
+    public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+}
